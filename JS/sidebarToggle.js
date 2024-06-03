232ff2ec8d2ed8e0ef9cab1809a2006da8dbd18a
theAmirMahdi <addstoreteam@gmail.com>
2024-06-04 00:21:24 +0330

    v1 - Add Button in function

--- JS/sidebarToggle.js
+++ JS/sidebarToggle.js
@@ -4,3 +4,4 @@ let toggleBtn = document.querySelector(".toggle-btn");
 toggleBtn.addEventListener("click", () => {
   sidebar.classList.toggle("active");
 });
+
---
+++ JS/weekoption.js
@@ -0,0 +1,5 @@
+let weekBtn = document.querySelector(".week-func button");
+
+weekBtn.addEventListener("click", () => {
+  console.log("clicked");
+});
